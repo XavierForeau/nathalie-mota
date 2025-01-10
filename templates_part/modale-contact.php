7f5a0f0647aeab01a3911ca612054efe66120415
Xavier Foreau <xavier.foreau.ocr@laposte.net>
2025-01-10 11:22:37 +0100

    Bannières / header / functions / scripts

--- templates_part/modale-contact.php
+++ templates_part/modale-contact.php
@@ -5,7 +5,7 @@
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-header.png" alt="contact header">
         <?php
         // On insère le formulaire de contact
-        echo do_shortcode('[contact-form-7 id="8b15adb" title="Formulaire de contact"]');
+        echo do_shortcode('[contact-form-7 id="50e16f7" title="Formulaire de contact 1"]');
         ?>
         </div>
     </div>
